12068ae9890799efc6816e803f67a612ea63b7cb
Manvir-Punglia <116977947+Manvir-Punglia@users.noreply.github.com>
2024-10-20 20:22:58 -0400

    Setup singleton abstract class, extended to timeManager, GameManager. Set up TimeManager UI and functionality

---
+++ EngineDesignAssignment1/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : Singleton<TimeManager>
+{
+    public float startTimeInSeconds = 10f;  
+    private float currentTime;
+    private bool isCountingDown = false;
+    public Text text;  
+
+    void Start()
+    {
+        ResetTimer();  
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+           
+            ResetTimer();
+        }
+    }
+
+    IEnumerator StartDeathCountdown()
+    {
+        isCountingDown = true;
+
+        while (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+
+            text.text = Mathf.Ceil(currentTime).ToString();
+           
+            yield return null; 
+        }
+
+        currentTime = 0;
+        text.text = "0";
+        isCountingDown = false;
+
+        CountDownEnd();  //execute when the timer hits zero
+    }
+
+    //called when the timer hits zero
+    void CountDownEnd()
+    {
+        Debug.Log("Timer has finished!");
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = startTimeInSeconds;
+
+        if (!isCountingDown)
+        {
+            StartCoroutine(StartDeathCountdown());
+        }
+    }
+
+}
+
